aba03e4bbc99ad438c5b9f32d57cd6c25bb77a72
YE <youssef.ejjiyar-etu@etu.univh2c.ma>
2024-01-09 21:29:01 +0100

    adding header

--- src/main/java/com/jeeproject/renocraft/web/controller/RenoCraftController.java
+++ src/main/java/com/jeeproject/renocraft/web/controller/RenoCraftController.java
@@ -1,4 +1,4 @@
-package com.jeeproject.renocraft.controller;
+package com.jeeproject.renocraft.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
