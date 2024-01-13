448ed338fb8fe84ab90790184545a9f663f3a336
YE <youssef.ejjiyar-etu@etu.univh2c.ma>
2024-01-13 14:42:57 +0100

    adding username

--- src/main/java/com/jeeproject/renocraft/web/controller/RenoCraftController.java
+++ src/main/java/com/jeeproject/renocraft/web/controller/RenoCraftController.java
@@ -256,6 +256,7 @@ public class RenoCraftController {
     public String getDashHome(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession();
         String user = (String) session.getAttribute("userName");
+        model.addAttribute("userNameDash", user);
         if (session != null && session.getAttribute("connexion") != null && (boolean) session.getAttribute("connexion")&& user.equals("admin")) {
             return "Dash/dashHome";
         } else {
