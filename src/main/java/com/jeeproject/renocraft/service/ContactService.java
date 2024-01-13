2e4a198c116ff1953c7c29632d6972eb7619d0e4
Yousra BARHMI <barhmiyousra@gmail.com>
2024-01-13 19:35:53 +0100

    page Contact added

--- src/main/java/com/jeeproject/renocraft/service/ContactService.java
+++ src/main/java/com/jeeproject/renocraft/service/ContactService.java
@@ -2,6 +2,9 @@ package com.jeeproject.renocraft.service;
 
 import com.jeeproject.renocraft.entity.Contact;
 
+import java.util.List;
+
 public interface ContactService {
     public void addContact(Contact contact);
+    public List<Contact> getContact();
 }
